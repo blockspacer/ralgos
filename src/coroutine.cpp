208834b89769fa47f772a7fcfa572d9cf2cb407b
terrywh <terry.wuhao@gmail.com>
2019-05-28 13:07:48 +0800

    重构:多线程响应模式,利用多线程计算单线程IO响应; 修正:线程数量由于单字节类型识别错误; 新增: PING 指令正确响应;

--- src/coroutine.cpp
+++ src/coroutine.cpp
@@ -10,6 +10,7 @@ coroutine_handler::coroutine_handler(std::shared_ptr<coroutine> co)
 }
 
 coroutine_handler& coroutine_handler::operator[](boost::system::error_code& error) {
+    assert(co_->id_ == std::this_thread::get_id());
     error_.reset(&error, boost::null_deleter());
     return *this;
 }
@@ -17,7 +18,8 @@ coroutine_handler& coroutine_handler::operator[](boost::system::error_code& erro
 void coroutine_handler::resume() {
     // 恢复实际执行的上下文，可能需要对应执行器（线程）
     boost::asio::post(co_->ex_, [co = co_] () {
-        coroutine::current = co;
+        assert(co->id_ == std::this_thread::get_id());
+        // coroutine::current = co;
         co->c1_ = std::move(co->c1_).resume();
     });
 }
@@ -35,9 +37,10 @@ void coroutine_handler::reset() {
 }
 
 void coroutine_handler::suspend() {
+    assert(co_->id_ == std::this_thread::get_id());
     // 一般当前上下文位于当前执行器（线程）
-    coroutine::current.reset();
+    // coroutine::current.reset();
     co_->c2_ = std::move(co_->c2_).resume();
 }
 
-std::shared_ptr<coroutine> coroutine::current;
+// std::shared_ptr<coroutine> coroutine::current;
--- src/handler/dummy.h
+++ src/handler/dummy.h
@@ -3,5 +3,5 @@
 #include "../handler.h"
 
 class handler_dummy: public handler, public std::enable_shared_from_this<handler_dummy> {
-    virtual void handle(std::shared_ptr<redisReply> req, std::shared_ptr<session> res, coroutine_handler& ch);
+    virtual void run(std::shared_ptr<redisReply> req, std::shared_ptr<session> res, coroutine_handler& ch);
 };
